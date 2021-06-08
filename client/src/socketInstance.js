2f9299014537ed14ce33492b953fe96a8d33912a
austin <austinlee199723@yahoo.com>
2021-06-08 15:20:39 -0700

    editing express static

--- client/src/socketInstance.js
+++ client/src/socketInstance.js
@@ -2,5 +2,5 @@ import io from "socket.io-client";
 export let getSocket = io("https://chat-app-tan.vercel.app");
 //localhost:5500
 export const getNewSocket = () => {
-  getSocket = io("");
+  getSocket = io("https://chat-app-tan.vercel.app");
 };
